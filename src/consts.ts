801bade01bdbffe03dcb6c92b649050dc1a0c5e3
mikishou1 <mikishou1@gmail.com>
2026-01-10 11:24:21 +0900

    デザイン改善

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'AI株価分析レポート';
+export const SITE_DESCRIPTION = 'AIが生成する日々の株式市場レポートと分析';
